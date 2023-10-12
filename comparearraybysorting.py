2c30430e3c54a90860208864e8f6fe857b23c680
harsh <harshadipkamble14@gmail.com>
2023-10-12 21:54:28 +0530

    array comparision by sorting

---
+++ comparearraybysorting.py
@@ -0,0 +1,15 @@
+def comparearray(arr1,arr2):
+    if len(arr1)!=len(arr2):
+        return False
+    else:
+        for i in range(len(arr1)):
+            if arr1[i]!=arr2[i]:
+                return False
+        return True
+arr1=[4,2,5,4,2,7,8]
+arr2=[4,2,5,4,2,7,8]
+result=comparearray(arr1,arr2)
+if result==True:
+    print("same arry!!!")
+else:
+    print("not same arry!!!")
--- harsh.py
+++
@@ -1,1 +0,0 @@
-print("harshadip haridas kamble..")
