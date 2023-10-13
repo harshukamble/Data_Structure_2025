09531c504032d132c0226f4b5fbf10c7cf2aa5e7
harsh <harshadipkamble14@gmail.com>
2023-10-13 14:18:15 +0530

    numpy array

---
+++ numpy_array.py
@@ -0,0 +1,9 @@
+import numpy as np
+arry1=np.array([[1,2,3],
+                [1,2,3]])
+print(arry1)
+print(arry1.ndim)
+print(arry1.size)
+print(arry1.shape)
+print(type(arry1))
+print(arry1.dtype)
